f24483cacc62c8b45a1388cc9585fe58d5ff1ab2
Matheus Teixeira <149537799+Matheuscrf@users.noreply.github.com>
2024-06-23 00:20:05 -0300

    melhorando o css da parte de cadastro/register

--- register.js
+++ register.js
@@ -1,7 +1,8 @@
-function validarFormulario() {
+function validarFormulario(event) {
+    event.preventDefault();
+
     var nome = document.getElementById('nome').value;
     var dataNascimento = document.getElementById('dataNascimento').value;
-    var sexo = document.getElementById('sexo').value;
     var nomeMaterno = document.getElementById('nomeMaterno').value;
     var cpf = document.getElementById('cpf').value;
     var celular = document.getElementById('celular').value;
@@ -10,13 +11,14 @@ function validarFormulario() {
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
     var confirmaSenha = document.getElementById('confirmaSenha').value;
+    var gender = document.querySelector('input[name="gender"]:checked');
     var message = document.getElementById('message');
     var success = document.getElementById('success');
 
     message.style.display = 'none';
     success.style.display = 'none';
 
-    if (!nome || !dataNascimento || !sexo || !nomeMaterno || !cpf || !celular || !fixo || !endereco || !login || !senha || !confirmaSenha) {
+    if (!nome || !dataNascimento || !nomeMaterno || !cpf || !celular || !fixo || !endereco || !login || !senha || !confirmaSenha || !gender) {
         message.innerText = "Todos os campos são obrigatórios.";
         message.style.display = 'block';
         return false;
@@ -28,7 +30,6 @@ function validarFormulario() {
         return false;
     }
 
-    
     var telefoneRegex = /^\(\+55\)\d{2}-\d{8,9}$/;
     if (!telefoneRegex.test(celular) || !telefoneRegex.test(fixo)) {
         message.innerText = "Os telefones devem estar no formato (+55)XX-XXXXXXXX.";
@@ -36,14 +37,12 @@ function validarFormulario() {
         return false;
     }
 
-    
     if (login.length !== 6 || !/^[a-zA-Z]+$/.test(login)) {
         message.innerText = "O login deve ter exatamente 6 caracteres alfabéticos.";
         message.style.display = 'block';
         return false;
     }
 
-    
     if (senha.length !== 8 || !/^[a-zA-Z]+$/.test(senha)) {
         message.innerText = "A senha deve ter exatamente 8 caracteres alfabéticos.";
         message.style.display = 'block';
@@ -61,7 +60,7 @@ function validarFormulario() {
 
     success.style.display = 'block';
     setTimeout(() => {
-        window.location.href = 'index.html'; 
+        window.location.href = 'index.html';
     }, 2000);
 
     return false;
